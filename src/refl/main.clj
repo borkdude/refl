40b71b9a702b5423e748d32ac4d48d72c5cf5a04
Rick Moynihan <rick.m@swirrl.com>
2021-06-16 22:25:33 +0100

    Change example to require multiple passes to instrument reflection
    Running:
    
    $ ./refl.main string
    
    Will work and correctly output
    
    ```
    3
    [Ljava.lang.String;
    ```
    
    However this is only because the reflection is instrumented with an
    argument of "string" that executes that the path
    
    The alternative branch however is not instrumented and running
    
    $ ./refl.main file
    
    Raises the exception:
    
    Exception in thread "main" java.lang.IllegalArgumentException: No matching field found: getPath for class java.io.File
    	at clojure.lang.Reflector.getInstanceField(Reflector.java:397)
    	at clojure.lang.Reflector.invokeNoArgInstanceMember(Reflector.java:440)
    	at refl.main$_main.invokeStatic(main.clj:19)
    	at refl.main$_main.invoke(main.clj:11)
    	at clojure.lang.AFn.applyToHelper(AFn.java:154)
    	at clojure.lang.AFn.applyTo(AFn.java:144)
    	at refl.main.main(Unknown Source)
    
    To avoid this we will need to instrument with multiple arguments and
    merge the reflect-config.json files.

--- src/refl/main.clj
+++ src/refl/main.clj
@@ -8,9 +8,12 @@
 (defn file [f]
   (io/file f))
 
-(defn -main [& _]
+(defn -main [arg]
   (let [res (refl-str "foo")]
-    (println (.length res)) ;; reflect on string
-    (prn (type (into-array [res res]))) ;; make array of unknown type
-    (println (.getPath (file "."))))) ;; reflect on file
+    (if (= arg "string")
+      (do (println (.length res))
+          (prn (type (into-array [res res])));; make array of unknown type) ;; reflect on string
 
+          )
+      ;; reflect on file
+      (println (.getPath (file "."))))))
