1975400db9822bd236d70175dc19c068022ff77a
Rick Moynihan <rick.m@swirrl.com>
2021-06-16 22:52:53 +0100

    DRY up classpath definition & make compile script a function
    Changed compile.clj to define a function so we can pass classpath
    directly.

--- script/compile.clj
+++ script/compile.clj
@@ -1,18 +1,16 @@
 #!/usr/bin/env bb
 
 (ns compile
-  (:require [babashka.process :refer [process sh]]
-            [clojure.java.io :as io]
-            [clojure.string :as str]))
+  (:require [babashka.process :refer [process]]
+            [clojure.java.io :as io]))
 
-(def classpath (str/trim (str/join ":" ["classes" (:out (sh "clojure -Spath"))])))
-
-@(process [(str (io/file (System/getenv "GRAALVM_HOME") "bin" "native-image"))
-           "-cp" classpath
-           "--initialize-at-build-time"
-           "--no-server"
-           "--no-fallback"
-           "-H:ReflectionConfigurationFiles=reflect-config-cleaned.json"
-           "-H:+ReportExceptionStackTraces"
-           "refl.main"]
-          {:inherit true})
+(defn compile-native-image [classpath]
+  @(process [(str (io/file (System/getenv "GRAALVM_HOME") "bin" "native-image"))
+             "-cp" classpath
+             "--initialize-at-build-time"
+             "--no-server"
+             "--no-fallback"
+             "-H:ReflectionConfigurationFiles=reflect-config-cleaned.json"
+             "-H:+ReportExceptionStackTraces"
+             "refl.main"]
+            {:inherit true}))
